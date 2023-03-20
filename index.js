a7e68d2c081e1124c8927e459aabf46a7a99154f
Brian Cheruiyot Rono <cheruiyotbrian97@gmail.com>
2023-03-20 06:25:55 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(number) {
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if(number<=400) {
+    return 'This one is on me!';
+  }
+  else if(number>2000 && number<=2499) {
+    return ('I will gladly take your thirty bucks.')
 }
+  else if(number>=2500) {
+    return ('No can do.');
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
+function ternaryCheckCity(Place){
+  
+  return (Place === 'NYC' ? 'Ok, sounds good.' : "No go.");
+}
+function switchOnCharmFromTip (Handout){
+  switch(Handout) {
+    case'generous' : return 'Thank you so much.';
+    break;
+    case'not as generous' : return 'Thank you.';
+    break;
+    default : return 'Bye.';
+
+  }}
